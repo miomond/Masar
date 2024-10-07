1abb7c141bcd5b06418c124930c08a408203aeaa
omar mostafa <eng.omarmostafa22@gmail.com>
2024-10-07 22:54:13 +0300

    ssss

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { Route } from "react-router-dom";
-import { useState } from "react";
 import "./App.css";
 import { Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Subscribtion from "./Pages/Subscribtion";
-import { NavB } from "./common/NavB";
 import StudentLogin from "./Pages/StudentLogin";
 import StudentRegister from "./Pages/StudentRegister";
 import TeacherLogin from "./Pages/TeacherLogin";
@@ -20,47 +18,44 @@ import TSittings from "./Pages/TeacherLayout/TSittings";
 import { AuthProvider, useAuth } from "./context/auth";
 import { RequireAuth } from "./context/RequireAuth";
 import Homy from "./Pages/Homy";
-import { useScroll } from "framer-motion";
 import SingleEvent from "./Pages/SingleEvent";
 import EventsPage from "./Pages/Events";
+import Layout from "./components/Layout/layout";
 
 function App() {
-  let auth = useAuth();
-
   return (
     <>
       <AuthProvider>
         {/*  */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="Homy" element={<Homy />} />
-          <Route path="about" element={<About />} />
-          <Route path="events" element={<EventsPage />} />
-          <Route path="eventsingle" element={<SingleEvent />} />
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="Homy" element={<Homy />} />
+            <Route path="about" element={<About />} />
+            <Route path="events" element={<EventsPage />} />
+            <Route path="eventsingle" element={<SingleEvent />} />
 
-          <Route path="Subscribtion" element={<Subscribtion />} />
-          {/* registering and loging  */}
-          <Route path="StudentLogin" element={<StudentLogin />} />
-          <Route path="StudentRegister" element={<StudentRegister />} />
-          <Route path="TeacherLogin" element={<TeacherLogin />} />
-          <Route path="TeacherRegister" element={<TeacherRegister />} />
-          {/* student layout */}
-          <Route path="CourseDetails" element={<CourseDetails />} />
-          <Route path="LessonVideo" element={<LessonDetails />} />
-          {/* Teacher */}
-          <Route
-            path="/Teacherprofile"
-            element={
-              <RequireAuth>
-                <TeacherLay />
-              </RequireAuth>
-            }
-          >
-            <Route path="profile" element={<TProfile />} />
-            <Route path="cources" element={<Tcources />} />
-            <Route path="Sittings" element={<TSittings />} />
-          </Route>
-        </Routes>
+            <Route path="Subscribtion" element={<Subscribtion />} />
+            <Route path="StudentLogin" element={<StudentLogin />} />
+            <Route path="StudentRegister" element={<StudentRegister />} />
+            <Route path="TeacherLogin" element={<TeacherLogin />} />
+            <Route path="TeacherRegister" element={<TeacherRegister />} />
+            <Route path="CourseDetails" element={<CourseDetails />} />
+            <Route path="LessonVideo" element={<LessonDetails />} />
+            <Route
+              path="/Teacherprofile"
+              element={
+                <RequireAuth>
+                  <TeacherLay />
+                </RequireAuth>
+              }
+            >
+              <Route path="profile" element={<TProfile />} />
+              <Route path="cources" element={<Tcources />} />
+              <Route path="Sittings" element={<TSittings />} />
+            </Route>
+          </Routes>
+        </Layout>
       </AuthProvider>
     </>
   );
---
+++ src/components/Layout/layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Layout({children}) {
+  return (
+    <div>
+    <header>fdgfghf</header>
+        {children}
+        footer
+    </div>
+  )
+}
+
+export default Layout
